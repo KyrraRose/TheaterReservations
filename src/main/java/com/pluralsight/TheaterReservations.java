9049b7eef301225f81468d6e76a6aec290711919
KyrraRose <krbosland1@gmail.com>
2025-09-29 15:13:03 -0400

    Got customer info, needs to output

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,7 +1,32 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class TheaterReservations {
     public static void main(String[] args) {
+        //New scanner!
+        Scanner mrMcScans=new Scanner(System.in);
 
+        //Welcome!
+        System.out.print("~ Welcome to Xanathar's Theater! ~\nPlease enter your full name: ");
+        String name = mrMcScans.nextLine();
+
+        //Date get
+        System.out.print("What is the date of the show (MM/DD/YY)?: ");
+        String userDate = mrMcScans.nextLine();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yy");
+        LocalDate showDate = LocalDate.parse(userDate,format);
+
+        //Number of tickets
+        System.out.print("How many tickets would you like?: ");
+        String tickets = mrMcScans.nextLine();
+        int numTickets = Integer.parseInt(tickets);
+
+        if (numTickets == 1){
+            System.out.println("1 ticket reserved for %");
+        }
     }
+
 }
